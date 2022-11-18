06bacd764f32cc6e7d3c8d8d2a5c55a566d469a6
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-11-18 09:35:59 +0000

    added navbar functionality and main functionality

--- src/index.js
+++ src/index.js
@@ -3,3 +3,23 @@
 import "./styles.css";
 import { upperNavLinks } from "./navbar";
 import { imageSlider } from "./image-slider";
+
+const themeOptions = (() => {
+	const themeToggle = document.querySelector(".theme-toggle");
+	const githubLogo = document.querySelector(".github-logo");
+
+	const changeTheme = () => {
+		if (document.documentElement.getAttribute(".theme-toggle") === "dark") {
+			document.documentElement.setAttribute("theme", "light");
+			themeToggle.setAttribute("aria-checked", "true");
+			githubLogo.setAttribute("src", "assets/images/logos/GitHub-Mark-32px.png")
+		} else {
+			document.documentElement.setAttribute("theme", "dark");
+			themeToggle.setAttribute("aria-checked", "false");
+			githubLogo.setAttribute("src", "assets/images/logos/GitHub-Mark-Light-32px.png");
+		};
+	};
+
+	changeTheme();
+	themeToggle.addEventListener("click", changeTheme);
+})
